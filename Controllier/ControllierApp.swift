f4a8113ce7c1aef6aab08ae514ede460737a80a7
Jared McFarland <jaredcolin@gmail.com>
2025-03-23 20:04:19 -0700

    Initial Commit

---
+++ Controllier/ControllierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllierApp.swift
+//  Controllier
+//
+//  Created by Jared McFarland on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ControllierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControllierTests/ControllierTests.swift
@@ -0,0 +1,17 @@
+//
+//  ControllierTests.swift
+//  ControllierTests
+//
+//  Created by Jared McFarland on 3/23/25.
+//
+
+import Testing
+@testable import Controllier
+
+struct ControllierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
